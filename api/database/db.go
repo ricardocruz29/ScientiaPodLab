5138e6288262baecf37ac91f58b757b30218dc2f
ricardocruz29 <ricardo.cruz2000@outlook.pt>
2023-05-08 11:43:03 +0100

    Created env file for data used to connect to db

--- api/database/db.go
+++ api/database/db.go
@@ -3,13 +3,22 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 
 func Connect() (*sql.DB, error) {
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/scientiapodlab_db")
+	user := os.Getenv("db_user")
+	password := os.Getenv("db_password")
+	host := os.Getenv("db_host")
+	port := os.Getenv("db_port")
+	name := os.Getenv("db_name")
+
+
+	dbCMD := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password,host, port, name)
+	db, err := sql.Open("mysql", dbCMD)
     if err != nil {
 			return nil, fmt.Errorf("error connecting to database: %v", err)
     }
--- api/main.go
+++ api/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/ricardocruz29/ScientiaPodLab/api/database"
 )
 
 // Watch this video to setup better: https://www.youtube.com/watch?v=jFfo23yIWac
 
 func main(){
+    //Load .env
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+  
+    //Connect to db
 	db, err := database.Connect()
     if err != nil {
         panic(err)
