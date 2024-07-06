4cb9f3d58af552e5fb737149e1b75207f37db7bc
ricardocruz29 <ricardo.cruz29@ua.pt>
2024-07-06 14:37:52 +0100

    refactor axios base query for multipart form data

--- client/scientiapodlab/src/api/axiosClient/axiosBaseQuery.js
+++ client/scientiapodlab/src/api/axiosClient/axiosBaseQuery.js
@@ -1,15 +1,26 @@
-import axiosInstance from "./axiosInstance";
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  withCredentials: true,
+});
 
 const axiosBaseQuery =
   ({ baseUrl }) =>
   async ({ url, method, data, params }) => {
     try {
+      const headers = {};
+      if (data instanceof FormData) {
+        headers["Content-Type"] = "multipart/form-data";
+      }
+
       const result = await axiosInstance({
         url: baseUrl + url,
         method,
         data,
         params,
+        headers,
       });
+
       return { data: result.data || {} }; // Return an empty object if no data
     } catch (axiosError) {
       let err = axiosError;
