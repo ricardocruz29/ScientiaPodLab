bae0cb08f3b0b7e7d8402d2088690a10b25bf7b3
ricardocruz29 <ricardo.cruz29@ua.pt>
2024-07-09 23:16:24 +0100

    Reusable components deliberation

--- client/scientiapodlab/src/App.js
+++ client/scientiapodlab/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Input } from "@mui/icons-material";
 
 function App() {
   //! Example on how to use useAuth hook to handle authentication
@@ -11,6 +12,22 @@ function App() {
   //   e.preventDefault();
   //   await authenticate({ email, password }, false); // false for register
   // };
+
+  //! Reusable Components
+  // card - templateCard - audioCard - episodeCard - size (large / medium (dont think will be used) / small);
+  // button - green - yellow - red - fillGreen;
+  // templateSequence - template;
+  // templateSequenceCard - type (Intro, Content, Outro, TTS, etc) - canListen - canRemove - audioName - state (Active -> When we are recording episode or Static -> When reviewing/listening episode);
+  // text - 16px - 18px
+  // title - 40px - 48px - 28px - 128px (and green)
+  // progressStepper - number - active
+  // Drag&Drop - MainPlaceholder - SecondaryPlaceholder - fullWidth
+  // Play button - size
+  // Tabs - optiosn - active tab
+  // Audio Wave
+  // Text Input
+  // Dropdown
+  // Text Area
 
   return (
     <div className="App">
