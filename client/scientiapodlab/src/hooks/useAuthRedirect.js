d2ccf33b2eb78ee6a037970afc09605b5e41ead6
ricardocruz29 <ricardo.cruz29@ua.pt>
2024-08-13 19:34:29 +0100

    Persiste login

--- client/scientiapodlab/src/hooks/useAuthRedirect.js
+++ client/scientiapodlab/src/hooks/useAuthRedirect.js
@@ -2,17 +2,29 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setUser } from "../redux/features/user/userSlice";
+import { useLazyGetUserQuery } from "../redux/api/services/userService";
 
 const useAuthRedirect = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const userInfo = useSelector((state) => state.user.information);
+  const [triggerGetUser] = useLazyGetUserQuery();
 
   useEffect(() => {
-    if (userInfo === undefined) {
-      navigate("/login");
-    }
-  }, [userInfo, dispatch, navigate]);
+    const checkAuthStatus = async () => {
+      if (userInfo === undefined) {
+        try {
+          const user = await triggerGetUser().unwrap();
+          dispatch(setUser({ info: user }));
+        } catch (error) {
+          console.error("User is not authenticated:", error);
+          navigate("/login");
+        }
+      }
+    };
+
+    checkAuthStatus();
+  }, [userInfo, dispatch, triggerGetUser, navigate]);
 };
 
 export default useAuthRedirect;
